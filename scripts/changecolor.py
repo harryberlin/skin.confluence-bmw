dafce0c3da49aa1dd1a76b8b2e1692d3b678d737
harryberlin <avr-ibus@gmx.net>
2022-12-17 22:25:57 +0100

    script fixes

--- scripts/changecolor.py
+++ scripts/changecolor.py
@@ -4,12 +4,32 @@
 # nice colorsets
 # http://www.computerhope.com/cgi-bin/htmlcolor.pl?c=BDBD3C
 
+import sys
+
+PY2 = sys.version_info[0] == 2
+    
 import xbmc
 import xbmcgui
 import xbmcaddon
 import re
 
+if PY2:
+    #del unicode_literals
+    from xbmc import translatePath as xbmc_translate_path
+    XBMC_LOG_LEVEL = xbmc.LOGNOTICE
+else:
+    from xbmcvfs import translatePath as xbmc_translate_path
+    XBMC_LOG_LEVEL = xbmc.LOGINFO
+
+
+ADDON_ID = 'skin.confluence-bmw'
 bDebug = False
+
+def log(msg):
+    if not PY2:
+        msg = str(msg).encode('utf-8', 'surrogateescape').decode('ISO-8859-1')
+    xbmc.log('%s: SRV: %s' % (ADDON_ID, msg), XBMC_LOG_LEVEL)
+
 
 # -- for skin --
 
@@ -17,29 +37,29 @@ def set_color(skinstring):
 
 
     Farben = {}
-    print xbmcaddon.Addon(xbmc.getSkinDir()).getAddonInfo('path').decode('utf-8')
+    log(bytes(xbmcaddon.Addon(xbmc.getSkinDir()).getAddonInfo('path'),'utf-8').decode('utf-8'))
     # quit()
-    xmlfile = xbmcaddon.Addon(xbmc.getSkinDir()).getAddonInfo('path').decode('utf-8') + '/colors/defaults.xml'
+    xmlfile = bytes(xbmcaddon.Addon(xbmc.getSkinDir()).getAddonInfo('path'),'utf-8').decode('utf-8') + '/colors/defaults.xml'
 
     with open(xmlfile, 'r') as xml_tags:
         xml = xml_tags.read()
 
-    print xml
+    log(xml)
     # xml='<?xml version="1.0" encoding="utf-8"?><colors><color name="white">FFFFFFFF</color><color name="grey">FFb4b4b4</color><color name="grey2">FF999999</color><color name="grey3">FF505050</color><color name="black">FF000000</color><color name="blue">FF0084ff</color><color name="selected">FFEB9E17</color><color name="bmwamber">FFFF7E00</color></colors>'
 
     match = re.findall('name="(.*?)" menutextcolor="true">(.*?)</', xml)
     for farbe, wert in match:
         Farben[farbe] = wert
-    print Farben
+    log(Farben)
     # print Farben['blue']
     Farbliste = sorted(Farben.keys())
     Wertliste = Farben.values()
     # [COLOR=blue]$INFO[Container(450).NumItems][/COLOR]
 
 
-    print 'Farbe 0: %s' % Farbliste[0]
-    print Farben[Farbliste[0]]
-    print Wertliste
+    log('Farbe 0: %s' % Farbliste[0])
+    log(Farben[Farbliste[0]])
+    log(Wertliste)
 
     # Farbliste.extend(['USERDEFINED'])
 
@@ -48,8 +68,8 @@ def set_color(skinstring):
 
     ret = xbmcgui.Dialog().select('Choose a Color', farbliste_tmp)
 
-    print ret
-    print 'selected: ' + farbliste_tmp[ret]
+    log(ret)
+    log('selected: ' + farbliste_tmp[ret])
     if bDebug:
         xbmcgui.Dialog().ok("Choosed color", farbliste_tmp[ret])
 
--- scripts/skin_update.py
+++ scripts/skin_update.py
@@ -10,13 +10,28 @@
 #url = 'https://github.com/harryberlin/skin.confluence-bmw/zipball/kodi_18'
 # https://github.com/harryberlin/skin.confluence-bmw/archive/ed9854b0a6329463aee099697d2d5dbd1acaef48.zip
 
+__author__ = 'harryberlin'
+
+import sys
+PY2 = sys.version_info[0] == 2
+
 import os
 import json
-import urllib, urllib2
+
+import xbmc, xbmcgui, xbmcaddon
+
+if PY2:
+    from urllib2 import urlopen
+    from urllib import urlretrieve
+    from xbmc import translatePath
+else:
+    from urllib.request import urlopen
+    from urllib.request import urlretrieve
+    from xbmcvfs import translatePath
+
 #import datetime
 import zipfile
 
-import xbmc, xbmcgui, xbmcaddon
 
 ADDON = xbmcaddon.Addon('skin.confluence-bmw')
 ADDON_NAME = ADDON.getAddonInfo('name')
@@ -24,12 +39,12 @@ ADDON_ID = ADDON.getAddonInfo('id')
 ADDON_ZIP_NAME = 'skin.confluence-bmw.zip'
 ADDON_URL = 'https://github.com/harryberlin/skin.confluence-bmw/archive/kodi_18.zip'
 ADDON_PATH = ADDON.getAddonInfo('path')
-ADDON_USER_PATH = os.path.join(xbmc.translatePath('special://userdata'), 'addon_data', ADDON_ID)
+ADDON_USER_PATH = os.path.join(translatePath('special://userdata'), 'addon_data', ADDON_ID)
 
 
 def _pbhook(numblocks, blocksize, filesize, url=None, dp=None):
     if dp.iscanceled():
-        print "Download Abgebrochen"
+        log("Download Abgebrochen")
         raise Exception("dialog canceled")
         #dp.close()
 
@@ -37,9 +52,12 @@ def _pbhook(numblocks, blocksize, filesize, url=None, dp=None):
         percent = min((numblocks * blocksize * 100) / filesize, 100)
         current_size = '%.*f' % (2, numblocks*blocksize/1024.0/1024.0)
         total_size = '%.*f' % (2, filesize/1024.0/1024.0)
-        print percent
+        log(percent)
         teststr = '%s / %s MB / %s%%' % (current_size, total_size, percent)
-        dp.update(int(10+percent*0.4), 'Downloading...', teststr, '%s%%' % int(10+percent*0.4))
+        if PY2:
+            dp.update(int(10+percent*0.4), 'Downloading...', teststr, '%s%%' % int(10+percent*0.4))
+        else:
+            dp.update(int(10 + percent * 0.4), 'Downloading...\n%s%%' % (teststr, int(10 + percent * 0.4)))
     except:
         pass
         #percent = 100
@@ -48,7 +66,7 @@ def _pbhook(numblocks, blocksize, filesize, url=None, dp=None):
 
 def is_internet_available():
     try:
-        urllib2.urlopen(ADDON_URL, timeout=5)
+        urlopen(ADDON_URL, timeout=5)
         #urllib2.urlopen('http://216.58.192.142', timeout=5)
         return True
     except:
@@ -64,26 +82,35 @@ def dialog_progressbar_timeout(dp, line_1, line_2, line_3, timeout=3000):
     for _ in range(100, 0, -1):
         if dp.iscanceled():
             break
-        dp.update(_, line_1, line_2, line_3)
-        xbmc.sleep(timeout/100)
+        if PY2:
+            dp.update(_, line_1, line_2, line_3)
+        else:
+            dp.update(_, '%s\n%s\n%s' % (line_1, line_2, line_3))
+        xbmc.sleep(int(timeout/100))
 
 
 def update(owner, repo, branch='master'):
     dp = xbmcgui.DialogProgress()
     dp.create('Updating %s' % ADDON_ID, 'Check Connection...')
-    dp.update(1, 'Check Connection...', ' ', '1%')
+    if PY2:
+        dp.update(1, 'Check Connection...', ' ', '1%')
+    else:
+        dp.update(1, 'Check Connection...\n \n1%')
 
     try:
 
         if not is_internet_available():
             raise Exception('No Internet connection')
-        dp.update(5, 'Check for new Version on github...', ' ', '5%')
+        if PY2:
+            dp.update(5, 'Check for new Version on github...', ' ', '5%')
+        else:
+            dp.update(5, 'Check for new Version on github...\n \n5%')
         try:
             url = 'https://api.github.com/repos/%s/%s/git/refs/heads/%s' % (owner, repo, branch)
-            res = json.loads(urllib2.urlopen(url).read())
+            res = json.loads(urlopen(url).read())
             log(res)
             sha_url = res['object']['url']
-            res = json.loads(urllib2.urlopen(sha_url).read())
+            res = json.loads(urlopen(sha_url).read())
             commit_date = res['committer']['date']
             try:
                 commit_message = res['message']
@@ -101,31 +128,39 @@ def update(owner, repo, branch='master'):
             if xbmc.getInfoLabel('Skin.String(github_sha)') == sha:
                 raise Exception('No Update required')
 
-        print 'downloading file'
+        log('downloading file')
         #download_file('https://github.com/harryberlin/skin.confluence-bmw/zipball/kodi_18', "%s\skin.confluence-bmw.zip" % (os.path.join('C:\\', 'temp')))
         #download_file("https://github.com/%s/%s/archive/%s.zip" % (owner, repo, sha), "%s\skin.confluence-bmw-%s.zip" % (os.path.join('C:\\', 'temp'), sha))
         #dialog = xbmcgui.Dialog()
         #if dialog.yesno("BMW RaspControl Skin Updater", "Are you sure to update the Skin?") == 1:
         #    pass
 
-        dp.update(10, 'Downloading...', ' ', '10%')
+        if PY2:
+            dp.update(10, 'Downloading...', ' ', '10%')
+        else:
+            dp.update(10, 'Downloading...\n \n10%')
         url = "https://github.com/%s/%s/archive/%s.zip" % (owner, repo, sha)
         #target_path = os.path.join(ADDON_USER_PATH, "skin.confluence-bmw-%s.zip" % sha)
         target_path = os.path.join(ADDON_USER_PATH, ADDON_ZIP_NAME)
 
         # https://www.programcreek.com/python/example/663/urllib.urlretrieve
 
-        urllib.urlretrieve(url, target_path, lambda nb, bs, fs, url=url: _pbhook(nb, bs, fs, url, dp))
+        urlretrieve(url, target_path, lambda nb, bs, fs, url=url: _pbhook(nb, bs, fs, url, dp))
 
-        dp.update(50, 'Check Zipfile...', ' ', '50%')
-
+        if PY2:
+            dp.update(50, 'Check Zipfile...', ' ', '50%')
+        else:
+            dp.update(50, 'Check Zipfile...\n \n50%')
         the_zip_file = zipfile.ZipFile(target_path, 'r')
         ret = the_zip_file.testzip()
 
         if ret is not None:
             raise Exception('Zipfile is bad')
 
-        dp.update(50, 'Checked Zipfile', ' ', '50%')
+        if PY2:
+            dp.update(50, 'Checked Zipfile', ' ', '50%')
+        else:
+            dp.update(50, 'Checked Zipfile\n \n50%')
 
         zip_count = 0
         zip_max_count = len(the_zip_file.infolist())
@@ -137,8 +172,10 @@ def update(owner, repo, branch='master'):
             if zipinfo.filename.startswith('skin.confluence-bmw'):
                 #%s' % os.path.split(zipinfo.filename)[1]
                 zipinfo.filename = zipinfo.filename.replace('skin.confluence-bmw-%s' % sha, '')
-                dp.update(int(50+int(float(zip_count)/float(zip_max_count)*100)*0.50), 'Extracting Files...', '%s / %s / %s%%' % (zip_count, zip_max_count, int(float(zip_count)/float(zip_max_count)*100)), '%s%%' % int(50+int(float(zip_count)/float(zip_max_count)*100)*0.50))
-
+                if PY2:
+                    dp.update(int(50+int(float(zip_count)/float(zip_max_count)*100)*0.50), 'Extracting Files...', '%s / %s / %s%%' % (zip_count, zip_max_count, int(float(zip_count)/float(zip_max_count)*100)), '%s%%' % int(50+int(float(zip_count)/float(zip_max_count)*100)*0.50))
+                else:
+                    dp.update(int(50 + int(float(zip_count) / float(zip_max_count) * 100) * 0.50), 'Extracting Files...\n%s / %s / %s%%\n%s%%' % (zip_count, zip_max_count, int(float(zip_count) / float(zip_max_count) * 100),int(50 + int(float(zip_count) / float(zip_max_count) * 100) * 0.50)))
                 try:
                     import test_skin
                     # for testing to don't overwrite myself
@@ -168,7 +205,10 @@ def update(owner, repo, branch='master'):
         #for _ in range(10,0,-1):
         #    dp.update(_*10, e.message, ' ', ' ')
         #    xbmc.sleep(300)
-        dialog_progressbar_timeout(dp, e.message, ' ', ' ', 7000)
+        if PY2:
+            dialog_progressbar_timeout(dp, e.message, ' ', ' ', 7000)
+        else:
+            dialog_progressbar_timeout(dp, e.args, ' ', ' ', 7000)
         #xbmc.executebuiltin("Notification(Skin Updater,%s,7000)" % e.message)
 
     finally:
